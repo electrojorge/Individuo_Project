e8bbd02ff8c60e83fdd978d5cb70e128bde06a5c
dariostes <carnicerdario@gmail.com>
2026-01-15 17:06:08 +0100

    GameManager

---
+++ Assets/Scripts/Player/Unit_Manager.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class Unit_Manager : MonoBehaviour
+{
+    //public Unit_SO unitData;
+
+    //string unitName;
+    //float unitHP;
+    //    public void SetHp(float hpMod)=> unitHP += hpMod;
+    //float unitPE;
+    //    public void SetPe(float peMod)=> unitPE += peMod;
+    //float unitEXP;
+    //    public void SetExp(float expMod)=> unitEXP += expMod;
+
+    //List<Ability_SO> abilities;
+
+    //public void SetAbility(bool add,Ability_SO ability)
+    //{
+    //    if (add)
+    //    {
+    //        abilities.Add(ability);
+    //    }
+    //    else if(abilities.Contains(ability))
+    //    {
+    //        abilities.Remove(ability);
+    //    }
+    //}
+
+    private void Start()
+    {
+        LoadData();
+    }
+
+    void LoadData()
+    {
+        //unitName = unitData.unitName;
+        //unitHP = unitData.unitHP;
+        //unitPE = unitData.unitPE;
+        //unitEXP = unitData.unitEXP;
+    }
+}
