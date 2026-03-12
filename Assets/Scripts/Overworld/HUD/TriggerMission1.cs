ff548839867f7c409c42a6a5d5cfa9f1b23fafff
dankie94 <cpa.a4336@cpasalduie.com>
2026-03-12 21:02:57 +0100

    misiones metidaas

---
+++ Assets/Scripts/Overworld/HUD/TriggerMission1.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TriggerMission1 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MissionManager.instance.EnemyAppears();
+        }
+    }
+}
