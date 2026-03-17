9a0f6f640fd3650ce3bd6fd21c5a07b5e50fc299
dankie94 <cpa.a4336@cpasalduie.com>
2026-03-17 20:52:03 +0100

    gg naisu / es mi main

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -8,10 +8,8 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject startScreen;
     public GameObject mainMenuPanel;
     public GameObject optionsMenuPanel;
-    public GameObject loadingMenuPanel;
 
     public Slider loadingSlider;
     public TextMeshProUGUI loadingText;
@@ -20,68 +18,13 @@ public class MainMenu : MonoBehaviour
 
     private void Start()
     {
-        startScreen.SetActive(true);
-        mainMenuPanel.SetActive(false);
-        optionsMenuPanel.SetActive(false);
-        loadingMenuPanel.SetActive(false);
-    }
-
-    private void Update()
-    {
-        if (Keyboard.current.anyKey.wasPressedThisFrame && !started)
-        {
-            started = true;
-            StartCoroutine(ShowMainMenuDelay());
-        }
-    }
-
-    IEnumerator ShowMainMenuDelay()
-    {
-        yield return new WaitForSeconds(0.25f);
-        startScreen.SetActive(false);
         mainMenuPanel.SetActive(true);
+        optionsMenuPanel.SetActive(false);
     }
 
     public void StartGameButton()
     {
-        StartCoroutine(StartGameDelay());
-    }
-
-    IEnumerator StartGameDelay()
-    {
-        mainMenuPanel.SetActive(false);
-        loadingMenuPanel.SetActive(true);
-
-        StartCoroutine(LoadingDots());
-
-        float duration = 5f;
-        float time = 0f;
-
-        while (time < duration)
-        {
-            time += Time.deltaTime;
-            loadingSlider.value = Mathf.SmoothStep(0, 1, time / duration);
-            yield return null;
-        }
-
-        loadingSlider.value = 1f;
-
-        StopCoroutine("LoadingDots");
-        loadingText.text = "Loading complete";
         SceneManager.LoadScene(3);
-    }
-
-    IEnumerator LoadingDots()
-    {
-        string baseText = "Loading game";
-        int dots = 0;
-
-        while (true)
-        {
-            dots = (dots + 1) % 4;
-            loadingText.text = baseText + new string('.', dots);
-            yield return new WaitForSeconds(0.5f);
-        }
     }
 
     public void OptionsButton()
