0074b8af572e510b4ea609bfcff9afcfee100e6b
dankie94 <cpa.a4336@cpasalduie.com>
2026-03-18 20:36:44 +0100

    sdasd

---
+++ Assets/Scripts/Overworld/HUD/PauseMenu.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject controllPauseMenu;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+        controllPauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.isPressed)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+     public void ExitToMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void RestarGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+    }
+
+    public void ControllMenu()
+    {
+        pauseMenu.SetActive(false);
+        controllPauseMenu.SetActive(true);
+    }
+
+    public void BackToPauseMenu()
+    {
+        controllPauseMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+}
